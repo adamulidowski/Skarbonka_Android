47c0b48fd6f0b46a74fa61ceb6107d6e6c34aa02
Adam Ulidowski <adam.ulidowski@wp.pl>
2017-05-08 22:59:25 +0200

    Wersja do oddania

--- app/src/main/java/com/example/adam/skarbonka/Chart.java
+++ app/src/main/java/com/example/adam/skarbonka/Chart.java
@@ -1,14 +1,19 @@
 package com.example.adam.skarbonka;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Chart extends AppCompatActivity {
 
@@ -16,33 +21,67 @@ public class Chart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
+        setTitle("Wykres");
 
+        List<AllHistory> history = getAllHistory();
+        ArrayList<Entry> entriess = new ArrayList<>();
+        for(int i=0; i<history.size(); i++){
+            entriess.add(new Entry(Integer.parseInt(history.get(i).getAktualnyStan()), i));
+        }
 
-        ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(4f, 0));
-        entries.add(new BarEntry(8f, 1));
-        entries.add(new BarEntry(6f, 2));
-        entries.add(new BarEntry(12f, 3));
-        entries.add(new BarEntry(18f, 4));
-        entries.add(new BarEntry(9f, 5));
+        LineDataSet datasett = new LineDataSet(entriess, "");
 
-        BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+        ArrayList<String> labelss = new ArrayList<String>();
 
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add("January");
-        labels.add("February");
-        labels.add("March");
-        labels.add("April");
-        labels.add("May");
-        labels.add("June");
+        for(int i=0; i<history.size(); i++){
+            labelss.add(history.get(i).getOpis());
+        }
 
-        BarChart chart = new BarChart(getBaseContext());
-        setContentView(chart);
+        LineChart chartt = new LineChart(getBaseContext());
+        setContentView(chartt);
 
-        BarData data = new BarData(labels, dataset);
-        chart.setData(data);
+        LineData dataa = new LineData(labelss, datasett);
 
-        chart.setDescription("Historia skarbonki");
+        datasett.setLineWidth(2);
+        //datasett.setColors(ColorTemplate.COLORFUL_COLORS);
+        datasett.setColor(Color.BLACK);
+        datasett.setCircleColor(Color.BLUE);
+        chartt.setData(dataa);
+        chartt.setScaleXEnabled(true);
+        chartt.setClickable(false);
+        chartt.setScaleYEnabled(false);
+        chartt.setDoubleTapToZoomEnabled(false);
+        chartt.setFocusable(false);
+        chartt.setFocusableInTouchMode(false);
+        chartt.dispatchWindowFocusChanged(false);
+        chartt.setPinchZoom(false);
+        chartt.setHorizontalScrollBarEnabled(true);
+        chartt.setVerticalScrollBarEnabled(true);
+        chartt.setDescription("Historia skarbonki");
+        chartt.setFocusable(false);
+        chartt.setFocusableInTouchMode(false);
+    }
 
+    public List<AllHistory> getAllHistory() {
+        List<AllHistory> allHistoryList = new ArrayList<>();
+        SQLiteDatabase baza = openOrCreateDatabase("skarbonka", MODE_PRIVATE, null);
+        Cursor cursor = baza.rawQuery("SELECT * FROM Wydatkii", null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                AllHistory ah = new AllHistory();
+                ah.setOpis(cursor.getString(cursor.getColumnIndex("Opis")));
+                ah.setAktualnyStan(cursor.getString(cursor.getColumnIndex("AktualnyStan")));
+                ah.setKwota(cursor.getString(cursor.getColumnIndex("Kwota")));
+                ah.setData(cursor.getString(cursor.getColumnIndex("Data")));
+                if(cursor.getString(cursor.getColumnIndex("czyPlus")).equals("+")){
+                    ah.setCzyPlus(true);
+                } else{
+                    ah.setCzyPlus(false);
+                }
+                allHistoryList.add(ah);
+            } while (cursor.moveToNext());
+        }
+        return allHistoryList;
     }
 }
